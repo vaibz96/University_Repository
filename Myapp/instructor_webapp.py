ae73fe929d2d476dce638df898b33376daf92e6a
vaibz96 <vshanbha@stevens.edu>
2019-04-24 13:49:56 -0400

    Flask_app files
    This branch consists of the jango file, flask file.
    The python code basically is a web page to display a summary of each Instructor with his/her CWID, Name, Department, Course, and the number of students in the course.

---
+++ Myapp/instructor_webapp.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import sqlite3
+import os 
+app = Flask(__name__)
+
+@app.route('/instructor_table')
+def instructore_table():
+
+    db = sqlite3.connect("C:\\Users\\shanb\\Database\\810_startup.db")
+
+    query = """ SELECT I.CWID, I.Name, I.Dept, G.Course, COUNT(G.Student_CWID) Students
+                FROM HW11_instructors I
+                    JOIN HW11_grades G ON  I.CWID = G.Instructor_CWID
+                GROUP BY G.COURSE
+                ORDER BY COUNT(G.Student_CWID) DESC"""
+
+    results = db.execute(query)
+
+    # convert the query into a list of dictionary to pass it into the template
+
+    data = [{ "cwid": cwid, "name": name, "dept": dept, "courses":courses, "count": count}\
+    for cwid, name, dept, courses, count in results]
+
+    return render_template('instructor_table.html',
+                        title = "Stevens Repository",
+                        table_title = "Number of students by course and instructor",
+                        instructors = data)
+
+app.run(debug = True)
